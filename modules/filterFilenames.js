757e86db2a82a01460fd7abb12b0c7d9ae62a210
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-02-01 12:06:58 -0300

    Delete filterFilenames.js

--- modules/filterFilenames.js
+++
@@ -1,8 +0,0 @@
-function filterFilenames(fileType, files){
-    let filteredFilenames;
-    return files[0].reduce((filteredNames, index)=>{
-       return filteredFilenames = files[0].filter(item => (new RegExp(fileType.toLowerCase())).test(item))
-    })
-}
-
-module.exports.filterFilenames = filterFilenames;
