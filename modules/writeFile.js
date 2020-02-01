d1d9311e7eac16eb021db28628bdf59d3d2f2db5
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-02-01 12:07:22 -0300

    Delete writeFile.js

--- modules/writeFile.js
+++
@@ -1,9 +0,0 @@
-const fileSystem  = require("fs");
-
-async function writeFile(outputFilePath, outputFileName, files){
-    const arrayOfFiles = []
-    files[0].map((FileList, index)=>{arrayOfFiles.push(`${files[0][index]}\n`)})
-    return fileSystem.writeFileSync(`${outputFilePath}/${outputFileName}`, arrayOfFiles, {enconding: 'utf-8'})
-}
-
-module.exports.writeFile = writeFile;
